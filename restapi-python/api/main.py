754da446724e2e041f21129fcaa7c4966a435875
monokkai <egormayer08@mail.ru>
2025-07-10 12:29:00 +0200

    dev: added new middleware for logs

--- restapi-python/api/main.py
+++ restapi-python/api/main.py
@@ -1,3 +1,4 @@
+import time
 from typing import Union
 from fastapi import FastAPI, Request
 
@@ -8,13 +9,27 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    duration = time.time() - start_time
+    print("------------------------------------")
+    print(f"{request.method}: {request.url}\n completed in: {duration:.2f}\n status: {response.status_code}")
+    print("------------------------------------")
+    return response
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, quantity: Union[str, None] = None):
     return {"item_id": item_id, "quantity": quantity}
 
+
 @app.get("/health")
 async def read_health():
     return {"healthy": True}
+
 
 @app.post("/echo")
 async def echo(request: Request):
