2da65748e19c0ce1338310d6f64869c6fe5ac1f9
monokkai <egormayer08@mail.ru>
2025-07-10 21:41:28 +0200

    dev: added new routes for checking auth and password

--- restapi-python/api/main.py
+++ restapi-python/api/main.py
@@ -1,10 +1,28 @@
-import time
 from typing import Union
-from fastapi import FastAPI, Request, Depends, HTTPException, security
+from fastapi import FastAPI, Request, Depends, HTTPException, security, Form
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from passlib.context import CryptContext
+import time
 
 app = FastAPI()
 security = HTTPBearer()
+
+SECRET_KEY = "secret_for_jwt"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+fake_users_db = {
+    "egor": {
+        "username": "egor",
+        "full_name": "Egor Mayer",
+        "hashed_password": pwd_context.hash("password123"),
+        "disabled": False,
+    }
+}
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
 
 @app.get("/")
 def read_root():
@@ -22,9 +40,9 @@ async def log_requests(request: Request, call_next):
     return response
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, quantity: Union[str, None] = None):
-    return {"item_id": item_id, "quantity": quantity}
+@app.get("/users/{user_id}")
+def read_item(user_id: int, quantity: Union[str, None] = None):
+    return {"user_id": user_id, "quantity": quantity}
 
 
 @app.get("/health")
@@ -37,9 +55,25 @@ async def echo(request: Request):
     data = await request.json()
     return {"You sent": data}
 
+
 @app.get("/secure")
 def read_secure(credentials: HTTPAuthorizationCredentials = Depends(security)):
     token = credentials.credentials
-    if token != "token11122334455":
+    if token != SECRET_KEY:
         raise HTTPException(status_code=403, detail="Forbidden")
     return {"message": "You have an access"}
+
+
+@app.post("/token")
+def get_token(
+        username: str = Form(..., description="User login"),
+        password: str = Form(..., description="User password")
+):
+    user = fake_users_db.get(username)
+    if not user:
+        raise HTTPException(status_code=400, detail="User not found")
+
+    if not verify_password(password, user["hashed_password"]):
+        raise HTTPException(status_code=400, detail="Invalid password")
+
+    return {"message": "User verified"}
