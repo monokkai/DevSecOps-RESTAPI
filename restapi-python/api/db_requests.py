3c79d5db6e99287d9f452106e1d8b3d6e2452653
monokkai <egormayer08@mail.ru>
2025-07-12 23:16:46 +0200

    dev: full CRUD for DB

--- restapi-python/api/db_requests.py
+++ restapi-python/api/db_requests.py
@@ -2,14 +2,6 @@ from sqlalchemy.orm import Session
 import models
 
 
-def get_user_by_username(db: Session, username: str):
-    return db.query(models.User).filter(models.User.username == username).first()
-
-
-def get_user_by_id(db: Session, id: int):
-    return db.query(models.User).filter(models.User.id == id).first()
-
-
 def create_user(db: Session, username: str, full_name: str, hashed_password: str):
     user = models.User(username=username, full_name=full_name, hashed_password=hashed_password)
     db.add(user)
@@ -17,6 +9,27 @@ def create_user(db: Session, username: str, full_name: str, hashed_password: str
     db.refresh(user)
     return user
 
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def get_user_by_id(db: Session, id: int):
+    return db.query(models.User).filter(models.User.id == id).first()
+
+def update_user(db: Session, id: int, username: str, full_name: str, hashed_password: str):
+    user = db.query(models.User).filter(models.User.username == username).first()
+    if not user:
+        return None
+
+    user.username = username
+    user.full_name = full_name
+    user.hashed_password = hashed_password
+
+    db.commit()
+    db.refresh(user)
+    return user
 
 def delete_user(db: Session, id: int):
     return db.query(models.User).filter(models.User.id == id).delete()
+
+
+
