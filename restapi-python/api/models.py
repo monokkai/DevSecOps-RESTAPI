14f0187bb71e67bc427d9dde906cbfba17bfd802
monokkai <egormayer08@mail.ru>
2025-07-12 23:05:25 +0200

    dev: made model for MySQL DB

---
+++ restapi-python/api/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from .database import base
+
+class User(base):
+    __tablename__ = 'Users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False)
+    full_name = Column(String(50))
+    hashed_password = Column(String(255), nullable=False)
+    disabled = Column(Boolean, default=False)
